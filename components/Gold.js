0ba6824882bd488957e55575713067469a9814ab
timbabnik <tim.babnik16@gmail.com>
2023-06-07 11:21:52 +0200

    dark-error

--- components/Gold.js
+++ components/Gold.js
@@ -6,7 +6,7 @@ function Writing({color, comment, desc, onClick, address, stylee}) {
 
   return (
 
-    <div onClick={onClick} style={stylee} className="writingThree" style={{borderWidth: border ? 1 : 1, borderColor: "lightgray"}}>
+    <div onClick={onClick} className="writingThree" style={{borderWidth: border ? 1 : 1, borderColor: "lightgray"}}>
         
             <div className="w-full flex flex-col">
               <div className="flex items-center pl-3  border-b border-gray-100 rounded-t-xl py-2 w-full">
